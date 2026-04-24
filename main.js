bbd8440c961a5f4173aead871ecbbc18c06e75fa
saudyapaulo <msaudyapaulo@gmail.com>
2026-04-24 19:03:24 +0200

    Remove tudo, mantém apenas model.js

--- main.js
+++
@@ -1,8 +0,0 @@
-//Inicialização: Criar instâncias e iniciar a aplicação
-
-// Aguardar o DOM carregar completamente
-document.addEventListener("DOMContentLoaded", function () {
-  const model = new LojaModel();
-  const view = new LojaView();
-  const controller = new LojaController(model, view);
-});
